5e47f40a02d6b3345176f0743f842873cbb3096f
tm-ahad <tm.ahad.07@gmail.com>
2023-12-30 20:37:24 +0600

    Logs added.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import type { ViteDevServer } from 'vite';
 import { defineConfig } from 'vite';
 
+const routes = [' /', ' /docs', ' /about']
+
+const middleWare = () => {
+	return  {
+		name: "middleware",
+		configureServer(server: ViteDevServer) {
+			server.middlewares.use((req, _, next) => {
+				console.log("Incoming request", req.originalUrl)
+				next()
+			})
+		}
+	}
+}
+
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), middleWare()],
 });
