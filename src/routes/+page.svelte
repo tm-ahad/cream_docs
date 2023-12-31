27be379e5f824eb0c3b4238ff0a94efff640cba9
tm-ahad <tm.ahad.07@gmail.com>
2023-12-31 11:06:46 +0600

    Index page updated.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 </div>
 
 <div class="section flex justify-center">
-    <h2>
+    <h2 class="text-lg">
         A transpiler for building blazingly fast web apps
     </h2>
 </div>
@@ -19,6 +19,9 @@
     </a>
     <a class="bg-blue-600 rounded-md mr-5 w-16 h-8 text-center pt-[3px]" href="/about">
         About
+    </a>
+    <a class="bg-blue-600 rounded-md mr-5 w-16 h-8 text-center pt-[3px]" href="/stdlib">
+        Stdlib
     </a>
 </div>
 
